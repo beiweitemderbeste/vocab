bf1a7052c2dd03b5f67984e1823ad4f6422adcb5
beiweitemderbeste <beiweitemderbeste@protonmail.com>
2024-07-03 23:17:20 +0200

    setting up react router

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,9 +1,15 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root") as Element);
+
+root.render(<RouterProvider router={router} />);
