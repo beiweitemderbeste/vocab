17303ef72d9e5e1fcc4d49a96240ef34ff670914
beiweitemderbeste <beiweitemderbeste@protonmail.com>
2024-07-05 17:04:52 +0200

    adding 404 page

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,12 +2,17 @@ import ReactDOM from "react-dom/client";
 
 import "./index.css";
 
+import NotFound from "./components/NotFound";
 import MainMenu from "./components/MainMenu";
 import SinglePlayer from "./components/SinglePlayer";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
+  {
+    path: "*",
+    element: <NotFound />,
+  },
   {
     path: "/",
     element: <MainMenu />,
