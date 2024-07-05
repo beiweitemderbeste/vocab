60689edff702aeb4632bf885ea57ef0f3cf58e3d
beiweitemderbeste <beiweitemderbeste@protonmail.com>
2024-07-05 17:19:01 +0200

    adding props to the topbar component

--- web/src/components/NotFound.tsx
+++ web/src/components/NotFound.tsx
@@ -5,7 +5,7 @@ import TopBar from "./TopBar";
 const NotFound = () => {
   return (
     <>
-      <TopBar />
+      <TopBar heading="vocab" />
       <div className="flex flex-col items-center justify-center h-screen">
         <h1 className="text-3xl font-bold mb-4">404 - Page Not Found</h1>
         <p className="text-lg text-gray-600">
