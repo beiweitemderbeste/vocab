f6c1e304c75acecd249deca5470c233a87297629
beiweitemderbeste <beiweitemderbeste@protonmail.com>
2024-07-05 13:22:53 +0200

    adding basic styling to the topbar component

--- web/src/components/TopBar.tsx
+++ web/src/components/TopBar.tsx
@@ -1,5 +1,11 @@
 const TopBar = () => {
-  return <p>hello from TopBar</p>;
+  return (
+    <div className="bg-purple-700 text-white p-4 shadow-lg">
+      <div className="container mx-auto flex justify-center items-center">
+        <h1 className="text-3xl font-bold">Vocab</h1>
+      </div>
+    </div>
+  );
 };
 
 export default TopBar;
