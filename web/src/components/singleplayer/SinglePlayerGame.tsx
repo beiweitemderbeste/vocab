9b1d56e7d44d40be1a06a77b4260ce69eeaa4879
beiweitemderbeste <beiweitemderbeste@protonmail.com>
2024-07-27 14:32:50 +0200

    adding supabase and database connection

--- web/src/components/singleplayer/SinglePlayerGame.tsx
+++ web/src/components/singleplayer/SinglePlayerGame.tsx
@@ -1,9 +1,23 @@
+import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
+
+import { supabase } from "../../supabaseClient";
 
 const SinglePlayerGame = () => {
   const location = useLocation();
   const { languagePair, difficultyLevel, wordCategory, wordRange, gameLength } =
     location.state;
+
+  const [englishAdjectives, setEnglishAdjectives] = useState([]);
+
+  useEffect(() => {
+    getEnglishAdjectives();
+  }, []);
+
+  async function getEnglishAdjectives() {
+    const { data } = await supabase.from("english_adjectives").select();
+    setEnglishAdjectives(data);
+  }
 
   return (
     <>
@@ -12,6 +26,11 @@ const SinglePlayerGame = () => {
       <p>word category: {wordCategory}</p>
       <p>word range: {wordRange}</p>
       <p>game length: {gameLength}</p>
+      <ul>
+        {englishAdjectives.map((adjective) => (
+          <li key={adjective.word}>{adjective.word}</li>
+        ))}
+      </ul>
     </>
   );
 };
